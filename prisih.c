a2c0eae747cc3cf7eedf3f44da9bec7e0120ca3e
prisihm <90698808+prisihm@users.noreply.github.com>
2021-10-26 16:29:30 +0300

    Add files via upload

---
+++ prisih.c
@@ -0,0 +1,25 @@
+
+/*
+Project : Library system
+Author : priscillah
+Date : 12/10/2021
+Language : C99
+
+
+**********************************/
+
+
+#include <stdlib.h>
+#include <windows.h>
+int main()
+{
+    printf("\tCounty Library System\t\t\t\n");
+    printf("Welcome\t\t\t\n");
+    printf("MENU\n");
+    printf("add books");
+    printf("return books");
+    printf("edit books");
+    printf("edit profile");
+
+    return 0;
+}
